f727d785a0a220e8e4c465a0801aaa1029678126
BackGwa <backgwa@icloud.com>
2023-08-31 00:23:44 +0900

    메세지 수정 및 콘텐츠 추가

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let history_text = `[대화 기록]\n`;
 
+
 async function fetchAIResponse(prompt) {
     const requestOptions = {
         method: 'POST',
@@ -9,24 +10,23 @@ async function fetchAIResponse(prompt) {
             'Authorization': `Bearer ${apiKey}`
         },
         body: JSON.stringify({
-            model: "gpt-3.5-turbo",
+            model: "gpt-3.5-turbo-16k",
             messages: [{
                 role: "system",
                 content: apiContent
             },
             {
                 role: "user",
-                content: history_text + `\n[현재 대화에 기반한 요청 및 응답]\n` + prompt
+                content: history_text + `\n[현재 대화에 기반한 요청]\n` + prompt
             } ],
-            temperature: 0.84,
-            max_tokens: 1000,
+            temperature: 0.8,
+            max_tokens: 786,
             top_p: 1,
-            frequency_penalty: 0.52,
-            presence_penalty: 0.5,
+            frequency_penalty: 0.51,
+            presence_penalty: 0.51,
             stop: ["Human"],
         }),
     };
-    console.log(history_text + `\n[현재 대화에 기반한 요청 및 응답]\n` + prompt);
     try {
         const response = await fetch(apiUrl, requestOptions);
         const data = await response.json();
@@ -58,8 +58,13 @@ function CallAI() {
     
     if (prompt.length > 0) {
 
+        if(!document.getElementById("start").classList.contains("hidden-image")) {
+            document.getElementById("start").classList.add("hidden-image");
+        }
+
         document.getElementById("userchat").value = "";
         document.getElementById("userchat").disabled = true;
+        document.getElementById("userchat").placeholder = "열심히 답변을 생성하고 있어요!"
 
         document.querySelector("chat-list").innerHTML = document.querySelector("chat-list").innerHTML + `<user-chat>${prompt.replace(/>/g, "〉").replace(/</g, "〈")}</user-chat>`;
         nowchat = document.querySelector("chat-list").innerHTML;
@@ -70,22 +75,26 @@ function CallAI() {
         fetchAIResponse(prompt)
           .then(response => {
                 if (response.length > 0) {
-                    document.querySelector("chat-list").innerHTML = nowchat + `<ai-chat>${response.replace(/\n/g, "<br>").replace(/>/g, "〉").replace(/</g, "〈")}</ai-chat>`;
+                    document.querySelector("chat-list").innerHTML = nowchat + `<ai-chat>${response.replace(/\n/g, "\newlineTag").replace(/>/g, "〉").replace(/</g, "〈").replace(/\newlineTag/g, "<br>")}</ai-chat>`;
                     document.getElementById("userchat").disabled = false;
+                    document.getElementById("userchat").placeholder = "메세지 보내기...";
                     document.querySelector("chat-list").scrollTop = document.querySelector("chat-list").scrollHeight;
                     history_text += "Human : " + prompt + "\n";
                     history_text += "SUITE GPT : " + response + "\n";
                 }
             })
           .catch(error => {
-                document.querySelector("chat-list").innerHTML = nowchat + `<ai-chat>오류 : ${error}</ai-chat>`;
+                console.error(error);
+                document.querySelector("chat-list").innerHTML = nowchat + `<ai-chat>오류가 발생했어!<br>몇 분만 기다렸다가, 시도해줘!</ai-chat>`;
                 document.getElementById("userchat").disabled = false;
+                document.getElementById("userchat").placeholder = "메세지 보내기...";
+                document.querySelector("chat-list").scrollTop = document.querySelector("chat-list").scrollHeight;
             });
     }
 }
 
 function clearChat() {
-    document.querySelector("chat-list").innerHTML = "";
+    document.querySelector("chat-list").innerHTML = '<img src="./res/start.png" id="start"><margin-chat></margin-chat>';
     history_text = `모든 대화는 초기화되었고, 처음부터 시작합니다. 사용자가 이전 대화에 대해 물어본다면, 대화가 초기화 되었다고 답하세요. 이를 참고하십시오.\n[대화 기록]\n`;
     call_alert("채팅을 모두 지웠습니다.");
 }
